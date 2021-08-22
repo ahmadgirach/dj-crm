4d6916826bddd68ed72143abdca7c93729a8c19c
Ahmad Girach <ahmedkhatri6@gmail.com>
2021-08-22 15:09:38 +0530

    [IMP/FMT] use dynamic urls instead of hard coding them in html and formatting

--- dj_crm/urls.py
+++ dj_crm/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('leads/', include('leads.urls')),
+    path("admin/", admin.site.urls),
+    path("leads/", include("leads.urls", namespace="leads")),
 ]
